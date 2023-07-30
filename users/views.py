3c4894eb1c5da946a8567e2da84456f4074a8de3
ChoiWheatley <chltmdgus604@naver.com>
2023-07-30 23:07:34 +0000

    notworking(login): login view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from rest_framework import viewsets
+from rest_framework import viewsets, views
 from rest_framework.response import Response
 from users.models import Member
 from users.serializers import MemberSerializer
@@ -7,7 +7,7 @@ from users.serializers import MemberSerializer
 
 class MemberViewSet(viewsets.ViewSet):
     """test view set example from https://www.django-rest-framework.org/api-guide/viewsets/#example"""
-    
+
     def list(self, request):
         queryset = Member.objects.all()
         serializer = MemberSerializer(queryset, many=True)
@@ -18,11 +18,17 @@ class MemberViewSet(viewsets.ViewSet):
         user = get_object_or_404(queryset, pk=pk)
         serializer = MemberSerializer(user)
         return Response(serializer.data)
-    
+
     def create(self, request):
+        """signup"""
         serializer = MemberSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.validated_data)
         return Response(serializer.errors)
-        
+
+
+class MemberLoginView(views.APIView):
+    def post(self, request):
+        serializer = MemberSerializer(data=request.data)
+
